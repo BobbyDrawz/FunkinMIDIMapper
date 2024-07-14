afeab1fe810a2def3f971f5ecf72c5bb196f885c
BobbyDrawz_ <veersparekh@outlook.com>
2024-07-14 16:25:18 -0400

    make the assembly info file

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Funkin' MIDI Mapper (FMM)")]
+[assembly: AssemblyDescription("A tool for converting MIDI files to FNF chart files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Funkin' MIDI Mapper (FMM)")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b04b83bd-d411-4f46-b56c-6f5cba4ce2cf")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
